261c5454ea84a8ff7f9b24a67a930c1a481ac60c
user@pavan-nayak <n220235@rguktn.ac.in>
2025-12-11 21:20:48 +0530

    requirments added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Eye Disease Chatbot",
+    version = "0.1.0",
+    author = "Pavan Nayak Vadthyavath",
+    author_email = "crazyking932455@gmail.com",
+    packages = find_packages(),
+    install_requirments = []
+)
