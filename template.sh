5e9f68f86ea0a43ed0b112904c130683abe3bd3c
user@pavan-nayak <n220235@rguktn.ac.in>
2025-12-11 20:03:25 +0530

    folder struture added

---
+++ template.sh
@@ -0,0 +1,14 @@
+#creating Directories
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch research/trial.ipynb
+touch requirments.txt
+
+echo "Project structure created successfully."
